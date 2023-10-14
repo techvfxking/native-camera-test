4628aeb09b91d185a0955aa52bf76146d4ee1baa
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-10-14 12:32:32 +0530

    Finally accessing native camera with Expo no need for react native cli DAnnnggg

--- App.tsx
+++ App.tsx
@@ -1,20 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react';
 
-export default function App() {
+function App() {
+  // The path of the picked image
+  const [pickedImagePath, setPickedImagePath] = useState('')
+
+  // This function is triggered when the "Select an image" button pressed
+  const showImagePicker = async () => {
+    // Ask the user for the permission to access the media library
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert("You've refused to allow this appp to access your photos!")
+      return
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync()
+
+    // Explore the result
+    console.log(result)
+
+    // if (!result.canceled) {
+    //   setPickedImagePath(result.uri)
+    //   console.log(result.uri)
+    // }
+  }
+
+  // This function is triggered when the "Open camera" button pressed
+  const openCamera = async () => {
+    // Ask the user for the permission to access the camera
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert("You've refused to allow this appp to access your camera!")
+      return
+    }
+
+    const result = await ImagePicker.launchCameraAsync()
+
+    // Explore the result
+    console.log(result)
+
+    // if (!result.canceled) {
+    //   setPickedImagePath(result.uri)
+    //   console.log(result.uri)
+    // }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.buttonContainer}>
+        <Button onPress={showImagePicker} title="Select an image" />
+        <Button onPress={openCamera} title="Open camera" />
+      </View>
+
+      <View style={styles.imageContainer}>
+        {/* {pickedImagePath !== '' && (
+          <Image source={{ uri: pickedImagePath }} style={styles.image} />
+        )} */}
+      </View>
     </View>
-  );
+  )
 }
 
+// Kindacode.com
+// Just some styles
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  buttonContainer: {
+    width: 400,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  imageContainer: {
+    padding: 30,
+  },
+  image: {
+    width: 400,
+    height: 300,
+    resizeMode: 'cover',
+  },
+})
+
+export default App;
+
